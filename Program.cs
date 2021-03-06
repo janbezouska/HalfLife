88ed52d8a77dbeac2987167290c731a7733614f6
janbezouska <bezouska.jan.2018@skola.ssps.cz>
2021-03-06 21:28:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fyzika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int jadra = 200; //začínáme s dvěstě jádry
+            
+            while(jadra > 0) //dokud zbývá alespoň jedno jádro, "házíme" znovu
+            {
+                jadra = Cycle(jadra);
+            }
+            Console.Read();
+            
+        }
+        static int Cycle(int _jadra)
+        {
+            Random rnd = new Random(); //náhodné číslo
+            int j;
+
+            for (int i = 0; i < _jadra; i++)
+            {
+                j = rnd.Next(1, 10); //náhodné číslo z <1, 10>
+                if (j < 6) //pokud je číslo menší než 6, pak se jádro přeměnilo
+                    _jadra--; //počet nepřeměněných jader se sníží o jedno
+            }
+            Console.WriteLine("Zbývající jádra: " + _jadra);
+
+            return _jadra;
+        }
+    }
+}
